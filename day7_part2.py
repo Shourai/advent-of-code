f605427fac92eaa4d490b732b65ecbf973a0430e
Shourai <10200748+Shourai@users.noreply.github.com>
2020-12-09 16:04:19 +0100

    Update day 7

--- day7_part2.py
+++ day7_part2.py
@@ -1,44 +1,32 @@
 import re
+import pprint as pp
 
 AoCInput = "test-input"
 
-colorList = []
+colorList = {}
 
 with open(AoCInput) as file:
     lines = file.readlines()
     for line in lines:
         if re.search(r'no other bags', line):
             m = line.split()
-            colorList.append({m[0] + ' ' + m[1]: None})
+            colorList[m[0] + ' ' + m[1]] = {}
         else:
             outside = re.search(r'(\w* \w*)', line)
             inside = re.findall(r'(\d)+ (\w* \w*)', line)
-            colorList.append(
-                {outside.group(): {i[1]: int(i[0]) for i in inside}})
+            colorList[outside.group()] = {i[1]: int(i[0]) for i in inside}
 
-# print(colorList)
-
-
-def findColor(color):
-    for i in colorList:
-        if list(i.keys())[0] == color:
-            color2 = i
-            print(color2)
-
-    for i in color2.values():
-        if i is None:
-            continue
-        for j in i.keys():
-            findColor(j)
+pp.pprint(colorList)
 
 
-# Find shiny gold bag
-for color in colorList:
-    if list(color.keys())[0] == "shiny gold":
-        shinyGold = color
+def countBags(color, colorList):
+    count = 1
+    insideBags = colorList[color]
+    for i in insideBags:
+        print(i)
+        multiply = insideBags[i]
+        count += multiply * countBags(i, colorList)
+    return count
 
-for i in shinyGold.values():
-    for j in i.keys():
-        findColor(j)
 
-# find color in list
+print(countBags('shiny gold', colorList))
