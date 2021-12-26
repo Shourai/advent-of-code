0d1e2b57eb0ed5f4c9eecddcb4014af5b187e942
Shourai <10200748+Shourai@users.noreply.github.com>
2021-12-26 20:32:43 +0100

    Add solution for day 12 part 1

--- day12/day12.py
+++ day12/day12.py
@@ -1,38 +1,41 @@
+from collections import defaultdict
+
+
 class Part1:
 
-    graph = {"start": ["A", "b"],
-             "A": ["c", "b"],
-             "b": ["d", "end"],
-             "c": ["A"],
-             "d": ["b"],
-             "end": ["A", "b"]}
-
-    n = len(graph)  # number of nodes in graph
-    visited = {"start": False,
-               "A": False,
-               "b": False,
-               "c": False,
-               "d": False,
-               "end": False}
+    graph = defaultdict(lambda: [])
+    count = 0
+    path = []
 
     def readInput(self):
-        with open("./day12/testinput", "r") as fd:
+        with open("./day12/input", "r") as fd:
             data = fd.read()
             data = data.splitlines()
-            print(data)
+
+        for line in data:
+            x, y = line.split("-")
+            self.graph[x].append(y)
+            self.graph[y].append(x)
+        print(self.graph)
 
     def dfs(self, node):
-        if self.visited[node]:
-            return
-        print(node)
-        self.visited[node] = True
+        self.path.append(node)
+
+        if node == "end":
+            print(self.path)
+            self.count += 1
 
         neighbours = self.graph[node]
         for next in neighbours:
-            self.dfs(next)
+            if next not in self.path or next.isupper():
+                self.dfs(next)
+
+        self.path.pop()
 
     def run(self):
+        self.readInput()
         self.dfs("start")
+        print(self.count)
 
 
 Part1().run()
