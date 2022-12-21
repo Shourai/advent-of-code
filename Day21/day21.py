dccbdcdb383fbcff57ce3a0b14d2dc1797715ba7
Shourai <10200748+Shourai@users.noreply.github.com>
2022-12-21 14:28:46 +0100

    Use imaginary number trick to solve part 2

--- Day21/day21.py
+++ Day21/day21.py
@@ -4,20 +4,54 @@ with open("./input", "r") as input:
     input = input.read().splitlines()
 
 
-numbers = dict()
-root = ""
+class Part1:
+    numbers = dict()
+    root = ""
 
-for line in input:
-    line = line.split(": ")
-    if "root" not in line:
-        numbers[line[0]] = line[-1]
-    else:
-        root = line[-1]
+    for line in input:
+        line = line.split(": ")
+        if "root" not in line:
+            numbers[line[0]] = line[-1]
+        else:
+            root = line[-1]
+
+    while any(re.findall(r"[a-z]{4}", root)):
+        match = re.findall(r"[a-z]{4}", root)
+        for m in match:
+            root = root.replace(m, "(" + numbers[m] + ")")
+
+    print("Part 1: ")
+    print(eval(root))
 
 
-while any(re.findall(r"[a-z]{4}", root)):
-    match = re.findall(r"[a-z]{4}", root)
-    for m in match:
-        root = root.replace(m, "(" + numbers[m] + ")")
+class Part2:
+    # Since humn only appears once and there is no multiplication
+    # between humn humn we can use imaginary numbers
+    numbers = dict()
+    rootA = ""
+    rootB = ""
 
-print(eval(root))
+    for line in input:
+        line = line.split(": ")
+        if "humn" in line:
+            numbers[line[0]] = "1j"
+        elif "root" in line:
+            rootA = line[-1].split()[0]
+            rootB = line[-1].split()[-1]
+        else:
+            numbers[line[0]] = line[-1]
+
+    while any(re.findall(r"[a-z]{4}", rootA)):
+        match = re.findall(r"[a-z]{4}", rootA)
+        for m in match:
+            rootA = rootA.replace(m, "(" + numbers[m] + ")")
+
+    while any(re.findall(r"[a-z]{4}", rootB)):
+        match = re.findall(r"[a-z]{4}", rootB)
+        for m in match:
+            rootB = rootB.replace(m, "(" + numbers[m] + ")")
+
+    print("Part 2: ")
+    print(round((eval(rootB)-eval(rootA).real)/eval(rootA).imag))
+    # print(eval(rootA))
+    # print(eval(rootB))
