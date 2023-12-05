b3d6d25e5de5e47cb90910b5f009e2df4d953a89
Shourai <10200748+Shourai@users.noreply.github.com>
2023-12-05 07:54:25 +0100

    Prepare for migration

--- readlines.py
+++
@@ -1,6 +0,0 @@
-with open('test-input') as file:
-    # newLines = [x.strip().split() for x in file.read().split('\n\n')]
-    data = [list(map(set, line.splitlines()))
-            for line in file.read().split('\n\n')]
-
-print(data)
