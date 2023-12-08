ab20d40ebd312ab38ac4d09487e01c69a9d99af5
Shourai <10200748+Shourai@users.noreply.github.com>
2023-12-08 22:34:42 +0100

    Rename data to grid

--- 2023/day_03/main.py
+++ 2023/day_03/main.py
@@ -1,4 +1,4 @@
-data = open(0).read().splitlines()
+grid = open(0).read().splitlines()
 
 # c: column
 # r: row
@@ -8,27 +8,27 @@ def part1():
     # Find the symbol coordinate, with top-left being (0,0).
     num_loc = set()
 
-    for row_idx, r in enumerate(data):
+    for row_idx, r in enumerate(grid):
         for col_idx, c in enumerate(r):
             if c.isdigit() or c == ".":
                 continue
             # Look around symbol
             for curr_row in [row_idx - 1, row_idx, row_idx + 1]:
                 for curr_col in [col_idx - 1, col_idx, col_idx + 1]:
-                    if curr_row < 0 or curr_row > len(data) or curr_col < 0 or curr_col > len(data[0]) or not data[curr_row][curr_col].isdigit():
+                    if curr_row < 0 or curr_row > len(grid) or curr_col < 0 or curr_col > len(grid[0]) or not grid[curr_row][curr_col].isdigit():
                         continue
-                    while curr_col > 0 and data[curr_row][curr_col - 1].isdigit():
+                    while curr_col > 0 and grid[curr_row][curr_col - 1].isdigit():
                         curr_col -= 1
                     num_loc.add((curr_row, curr_col))
     # print(num_loc)
 
     total = 0
-    print(len(data[0]))
+    print(len(grid[0]))
     for r,c in num_loc:
         number = ""
 
-        while c < len(data[r]) and data[r][c].isdigit():
-            number += data[r][c]
+        while c < len(grid[r]) and grid[r][c].isdigit():
+            number += grid[r][c]
             c += 1
         total += int(number)
     print(total)
