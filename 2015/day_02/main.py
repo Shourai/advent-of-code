c7ea616975599ec1ea386a3ad03f188343ba40a0
Shourai <10200748+Shourai@users.noreply.github.com>
2024-11-14 17:05:59 +0100

    Add 2015 day_02 part 1

---
+++ 2015/day_02/main.py
@@ -0,0 +1,29 @@
+from itertools import combinations
+
+
+def part1(data):
+    total = 0
+    for i in data:
+        l = i[0]
+        w = i[1]
+        h = i[2]
+        sides = []
+        sides.append(l * w)
+        sides.append(w * h)
+        sides.append(h * l)
+
+        min_side = min(sides)
+        total_surface = sum(map(lambda x: x * 2, sides))
+        total += total_surface + min_side
+    print(total)
+
+
+if __name__ == "__main__":
+    with open("./input", "r") as f:
+        # input = list(map(lambda x: x.split("x"),f.read().splitlines()))
+        data = f.read().splitlines()
+        lst = []
+        for i in data:
+            lst.append(list(map(int, i.split("x"))))
+
+    part1(lst)
