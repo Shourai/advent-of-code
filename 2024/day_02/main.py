c90b7750afa07c9fe67ce3894c96b5c2add89232
Shourai <10200748+Shourai@users.noreply.github.com>
2024-12-02 21:51:48 +0100

    Add day_02 part 2

--- 2024/day_02/main.py
+++ 2024/day_02/main.py
@@ -1,23 +1,47 @@
-with open("./testinput") as f:
+with open("./input") as f:
     data = []
     for line in f:
         data.append([int(i) for i in line.split()])
 
 
 def part1():
+    increasing_order = []
+    decreasing_order = []
     safe_counter = 0
-    for report in data:
-        d = []
-        for i in range(len(report) - 1):
-            d.append(report[i] - report[i + 1])
-        if all(i < 0 for i in d) or all(i > 0 for i in d):
-            if all(1 <= abs(i) <= 3 for i in d):
-                safe_counter += 1
+
+    for lvl in data:
+        if all(lvl[i] < lvl[i + 1] for i in range(len(lvl) - 1)):
+            increasing_order.append(lvl)
+        elif all(lvl[i] > lvl[i + 1] for i in range(len(lvl) - 1)):
+            decreasing_order.append(lvl)
+    for lvl in increasing_order:
+        if all((lvl[i + 1] - lvl[i]) in range(1, 4) for i in range(len(lvl) - 1)):
+            safe_counter += 1
+    for lvl in decreasing_order:
+        if all((lvl[i] - lvl[i + 1]) in range(1, 4) for i in range(len(lvl) - 1)):
+            safe_counter += 1
     print(safe_counter)
 
 
 def part2():
-    pass
+    safe_counter = 0
+
+    for lvl in data:
+        for j in range(len(lvl)):
+            dampened = lvl[:j] + lvl[j + 1 :]
+            if all(
+                (dampened[i + 1] - dampened[i]) in range(1, 4)
+                for i in range(len(dampened) - 1)
+            ):
+                safe_counter += 1
+                break
+            if all(
+                (dampened[i] - dampened[i + 1]) in range(1, 4)
+                for i in range(len(dampened) - 1)
+            ):
+                safe_counter += 1
+                break
+    print(safe_counter)
 
 
 if __name__ == "__main__":
