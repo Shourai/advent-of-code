b97f66e7675d59ed6426c8a7d17016ef55964411
Shourai <10200748+Shourai@users.noreply.github.com>
2024-12-05 21:43:43 +0100

    Refactor day_04 part 1

--- 2024/day_04/main.py
+++ 2024/day_04/main.py
@@ -1,87 +1,49 @@
 with open("./input", "r") as f:
-    data = [[".",".",".",". "] + list(x)[:-1] + [".",".",".",". "] for x in f.readlines()]
+    data = [[".", ".", "."] + list(x)[:-1] + [".", ".", "."] for x in f.readlines()]
 
 extra = len(data[0]) * ["."]
 
-data = 4 * extra + data + 4 * [extra] 
+data = 3 * [extra] + data + 3 * [extra]
+
 
 def part1():
+    mas_list = ["M", "A", "S"]
     occurances = 0
     for y in range(len(data)):
         for x in range(len(data[y])):
             if data[y][x] == "X":
-                try:
-                    if (
-                        data[y][x + 1] == "M"
-                        and data[y][x + 2] == "A"
-                        and data[y][x + 3] == "S"
-                    ):
-                        occurances += 1
-                except:
-                    pass
-                try:
-                    if (
-                        data[y][x - 1] == "M"
-                        and data[y][x - 2] == "A"
-                        and data[y][x - 3] == "S"
-                    ):
-                        occurances += 1
-                except:
-                    pass
-                try:
-                    if (
-                        data[y + 1][x] == "M"
-                        and data[y + 2][x] == "A"
-                        and data[y + 3][x] == "S"
-                    ):
-                        occurances += 1
-                except:
-                    pass
-                try:
-                    if (
-                        data[y - 1][x] == "M"
-                        and data[y - 2][x] == "A"
-                        and data[y - 3][x] == "S"
-                    ):
-                        occurances += 1
-                except:
-                    pass
-                try:
-                    if (
-                        data[y + 1][x + 1] == "M"
-                        and data[y + 2][x + 2] == "A"
-                        and data[y + 3][x + 3] == "S"
-                    ):
-                        occurances += 1
-                except:
-                    pass
-                try:
-                    if (
-                        data[y - 1][x + 1] == "M"
-                        and data[y - 2][x + 2] == "A"
-                        and data[y - 3][x + 3] == "S"
-                    ):
-                        occurances += 1
-                except:
-                    pass
-                try:
-                    if (
-                        data[y + 1][x - 1] == "M"
-                        and data[y + 2][x - 2] == "A"
-                        and data[y + 3][x - 3] == "S"
-                    ):
-                        occurances += 1
-                except:
-                    pass
-                try:
-                    if (
-                        data[y - 1][x - 1] == "M"
-                        and data[y - 2][x - 2] == "A"
-                        and data[y - 3][x - 3] == "S"
-                    ):
-                        occurances += 1
-                except:
-                    pass
+                if [data[y][x + 1], data[y][x + 2], data[y][x + 3]] == mas_list:
+                    occurances += 1
+                if [data[y][x - 1], data[y][x - 2], data[y][x - 3]] == mas_list:
+                    occurances += 1
+                if [data[y + 1][x], data[y + 2][x], data[y + 3][x]] == mas_list:
+                    occurances += 1
+                if [data[y - 1][x], data[y - 2][x], data[y - 3][x]] == mas_list:
+                    occurances += 1
+                if [
+                    data[y + 1][x + 1],
+                    data[y + 2][x + 2],
+                    data[y + 3][x + 3],
+                ] == mas_list:
+                    occurances += 1
+                if [
+                    data[y - 1][x + 1],
+                    data[y - 2][x + 2],
+                    data[y - 3][x + 3],
+                ] == mas_list:
+                    occurances += 1
+                if [
+                    data[y + 1][x - 1],
+                    data[y + 2][x - 2],
+                    data[y + 3][x - 3],
+                ] == mas_list:
+                    occurances += 1
+                if [
+                    data[y - 1][x - 1],
+                    data[y - 2][x - 2],
+                    data[y - 3][x - 3],
+                ] == mas_list:
+                    occurances += 1
     print(occurances)
 
 
@@ -90,46 +52,34 @@ def part2():
     for y in range(len(data)):
         for x in range(len(data[y])):
             if data[y][x] == "A":
-                try:
-                    if (
-                        data[y-1][x - 1] == "M"
-                        and data[y+1][x - 1] == "M"
-                        and data[y-1][x + 1] == "S"
-                        and data[y+1][x + 1] == "S"
-                    ):
-                        occurances += 1
-                except:
-                    pass
-                try:
-                    if (
-                        data[y-1][x - 1] == "M"
-                        and data[y+1][x - 1] == "S"
-                        and data[y-1][x + 1] == "M"
-                        and data[y+1][x + 1] == "S"
-                    ):
-                        occurances += 1
-                except:
-                    pass
-                try:
-                    if (
-                        data[y-1][x - 1] == "S"
-                        and data[y+1][x - 1] == "S"
-                        and data[y-1][x + 1] == "M"
-                        and data[y+1][x + 1] == "M"
-                    ):
-                        occurances += 1
-                except:
-                    pass
-                try:
-                    if (
-                        data[y-1][x - 1] == "S"
-                        and data[y+1][x - 1] == "M"
-                        and data[y-1][x + 1] == "S"
-                        and data[y+1][x + 1] == "M"
-                    ):
-                        occurances += 1
-                except:
-                    pass
+                if (
+                    data[y - 1][x - 1] == "M"
+                    and data[y + 1][x - 1] == "M"
+                    and data[y - 1][x + 1] == "S"
+                    and data[y + 1][x + 1] == "S"
+                ):
+                    occurances += 1
+                if (
+                    data[y - 1][x - 1] == "M"
+                    and data[y + 1][x - 1] == "S"
+                    and data[y - 1][x + 1] == "M"
+                    and data[y + 1][x + 1] == "S"
+                ):
+                    occurances += 1
+                if (
+                    data[y - 1][x - 1] == "S"
+                    and data[y + 1][x - 1] == "S"
+                    and data[y - 1][x + 1] == "M"
+                    and data[y + 1][x + 1] == "M"
+                ):
+                    occurances += 1
+                if (
+                    data[y - 1][x - 1] == "S"
+                    and data[y + 1][x - 1] == "M"
+                    and data[y - 1][x + 1] == "S"
+                    and data[y + 1][x + 1] == "M"
+                ):
+                    occurances += 1
     print(occurances)
 
 
