1c7b3808fa0223bc65b2a9ccc69fd8a5792a05fd
Shourai <10200748+Shourai@users.noreply.github.com>
2024-12-06 20:20:14 +0100

    Add day_06 part 2

--- 2024/day_06/main.py
+++ 2024/day_06/main.py
@@ -1,14 +1,16 @@
 import numpy as np
 import pprint
+import copy
 
 with open("./input", "r") as f:
     m = [["Y"] + list(x)[:-1] + ["Y"] for x in f]
 
-padding = [len( m[0] ) * ["Y"]]
+padding = [len(m[0]) * ["Y"]]
 
 m = padding + m + padding
 # pprint.pprint(m)
 
+n = copy.deepcopy(m)
 print("\n")
 
 
@@ -16,31 +18,47 @@ def rotate(m):
     return np.rot90(m, k=1)
 
 
-record_coords = []
-
-
 def walk(m):
     for y in range(len(m)):
         for x in range(len(m[y])):
             if m[y][x] == "^":
                 for i in range(1, len(m)):
-                    if m[y-i][x] == "Y":
-                        print("Reached edge")
-                        part1(m)
-                        exit()
-                    elif not (m[y - i][x] == "#" or m[y-i][x] == "Y"):
+                    if m[y - i][x] == "Y":
+                        return True
+                    elif not m[y - i][x] == "#":
                         m[y - i][x] = "^"
                         m[y - i + 1][x] = "X"
-                        record_coords.append((y - i, x))
                     else:
                         break
-def part1(m):
+
+
+def walk_with_obstacle(m, obstacle):
+    for y in range(len(m)):
+        for x in range(len(m[y])):
+            if m[y][x] == "^":
+                for i in range(1, len(m)):
+                    if m[y - i][x] == "Y":
+                        print("Reached edge")
+                        return True, 0
+                    if not (m[y - i][x] == "#" or m[y - i][x] == "O"):
+                        m[y - i][x] = "^"
+                        m[y - i + 1][x] = "X"
+                        if (y - i, x) not in obstacle:
+                            obstacle[(y - i, x)] = 1
+                        else:
+                            obstacle[(y - i, x)] += 1
+                    else:
+                        break
+    return False, obstacle
+
+
+def count_visited(m):
     count = 0
     for i in m:
         for j in i:
             if j == "X":
                 count += 1
-    pprint.pprint(m)
+    # pprint.pprint(m)
     print(count + 1)
 
 
@@ -49,10 +67,73 @@ def part2():
 
 
 if __name__ == "__main__":
-    while True:
-        walk(m)
+    # Part 1
+    rotations = 0
+    finished = False
+    while not finished:
+        finished = walk(m)
         m = rotate(m)
-    pprint.pprint(m)
+        rotations += 1
+    count_visited(m)
 
-    part1(m)
-    part2()
+    # Rotate to right direction
+    print("rotations: ", rotations)
+    r = rotations % 4
+    for _ in range(4 - r):
+        m = rotate(m)
+
+    # find all visited coordinates
+    record_coords = set()
+    for y in range(len(m)):
+        for x in range(len(m[y])):
+            if m[y][x] == "X" or m[y][x] == "^":
+                record_coords.add((y, x))
+    print(record_coords)
+
+    # Part 2
+
+    # Remove starting coordinate
+    starting_coord = None
+    for y in range(len(n)):
+        for x in range(len(n[y])):
+            if n[y][x] == "^":
+                starting_coord = (y, x)
+                record_coords.remove((y, x))
+
+    count = 0
+
+    for coord in record_coords:
+        # print(coord)
+        y, x = coord
+        newmap = copy.deepcopy(n)
+        newmap[y][x] = "O"
+        # pprint.pprint(newmap)
+        finished = False
+        obstacle = {starting_coord: 1}
+        while not finished:
+            if obstacle == 0:
+                continue
+            if max(obstacle.values()) > 4:
+                count += 1
+                break
+            finished, obstacle = walk_with_obstacle(newmap, obstacle)
+            newmap = rotate(newmap)
+    print(count)
+
+    # y,x = (4,9)
+    # newmap = copy.deepcopy(n)
+    # newmap[y][x] = "O"
+    # # pprint.pprint(newmap)
+    # finished = False
+    # obstacle = {starting_coord: 1}
+    # while not finished:
+    #     if obstacle == 0:
+    #         continue
+    #     if max(obstacle.values()) > 4:
+    #         count += 1
+    #         break
+    #     finished, obstacle = walk_with_obstacle(newmap, obstacle)
+    #     newmap = rotate(newmap)
+    # pprint.pprint(newmap)
+    # print(obstacle)
+    # print(count)
