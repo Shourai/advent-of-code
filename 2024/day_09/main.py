7a67fe176d943c53d720c34038a5bd2b32ccc497
Shourai <10200748+Shourai@users.noreply.github.com>
2024-12-15 10:48:36 +0100

    Add day_09 part 2

--- 2024/day_09/main.py
+++ 2024/day_09/main.py
@@ -1,4 +1,4 @@
-with open("./testinput2", "r") as f:
+with open("./input", "r") as f:
     data = list(map(int, list(f.read().split()[0])))
 
 
@@ -47,46 +47,55 @@ def file_compacting_p2(blocks):
     """
     Use two 'dynamic' sliding windows, one from the left and one from the right
     """
-    print(blocks)
+    # print(blocks)
 
     l1 = l2 = 0
     r1 = r2 = -1
 
-    left_window = []
-    right_window = []
+
     # Merged sliding window logic from both sides
-    i = 0
     j = len(blocks) - 1
 
-    while i <= j:
-        # Left sliding window (searching for gaps ".")
-        if blocks[i] == ".":
-            l1 = i
-            while i <= j and blocks[i] == ".":
-                i += 1
-            l2 = i
-            left_window.append(blocks[l1:l2])
-            print(
-                "Left Window:", l1, l2, blocks[l1:l2]
-            )  # Prints the left window with gaps
+    def search_gap(r1,r2):
+        i = 0
+        while i <= j:
+            # Left sliding window (searching for gaps ".")
+            if blocks[i] == ".":
+                l1 = i
+                while i <= j and blocks[i] == ".":
+                    i += 1
+                l2 = i
+                # print(
+                #     "Left Window:", l1, l2, blocks[l1:l2]
+                # )  # Prints the left window with gaps
 
+                if r1 - r2 <= l2 - l1:
+                    for m,n in zip(range(l1,l2), range(r2,r1)):
+                        blocks[m] = blocks[n]
+                        blocks[n] = "x"
+                    return
+            i += 1
+
+    while j > 0: # Make sure j > 0 before proceeding
         # Right sliding window (searching for same digits)
-        if blocks[j] != "." and (i <= j):  # Make sure i <= j before proceeding
+        if blocks[j] != "." and blocks[j] != "x":  
             r1 = j + 1  # Add 1 to adjust for the slicing location
-            while j >= i and blocks[j] != "." and blocks[j] == blocks[j - 1]:
+            while j >= 0 and blocks[j] != "." and blocks[j] == blocks[j - 1]:
                 j -= 1
             r2 = j
-            right_window.append(blocks[r2:r1])
-            print(
-                "Right Window:", r1, r2, blocks[r2:r1]
-            )  # Prints the right window with repeated digits
+
+            # print(
+            #     "Right Window:", r1, r2, blocks[r2:r1]
+            # )  # Prints the right window with repeated digits
+
+            search_gap(r1,r2)
 
         # Move towards the center
-        i += 1
         j -= 1
 
-    for i, j in zip(left_window, right_window):
-        print(i, j)
+    return blocks
+    # for i, j in zip(left_window, right_window):
+    #     print(i, j)
 
 
 def checksum(blocks):
@@ -100,9 +109,20 @@ def checksum(blocks):
 
     print(total)
 
+def checksum_p2(blocks):
+    """
+    Calculate the checksum
+    """
+    total = 0
+    for idx, c in enumerate(blocks):
+        if type(c) is int:
+            total += idx * c
+    print(total)
+
 
 if __name__ == "__main__":
     blocks = block_respresentation(data)
     # blocks = file_compacting_p1(blocks)
     blocks = file_compacting_p2(blocks)
+    checksum_p2(blocks)
     # checksum(blocks)
