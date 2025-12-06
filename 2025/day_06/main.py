d400aeec250d6c9d6accf0be2811b924ffaf2835
Shourai <10200748+Shourai@users.noreply.github.com>
2025-12-06 19:12:37 +0100

    Add comments

--- 2025/day_06/main.py
+++ 2025/day_06/main.py
@@ -1,9 +1,12 @@
 def part1():
+    import math
+
     with open("./testinput", "r") as file:
         input = [line.split() for line in file.readlines()]
 
+    # Put the input in the following format:
+    # {1: ["123", "45", "6", "*"], 2: ["328", "64", "98", +], ...}
     columns = {}
-
     for line in input:
         for idx in range(len(line)):
             if idx not in columns:
@@ -13,19 +16,22 @@ def part1():
     count = 0
 
     for col in columns.values():
-        count_multiplication = 1
-        count_addition = 0
+        multiplication_list = []
+        addition_list = []
         if col[-1] == "*":
             for i in range(len(col) - 1):
-                count_multiplication *= int(col[i])
+                multiplication_list.append(int(col[i]))
         elif col[-1] == "+":
             for i in range(len(col) - 1):
-                count_addition += int(col[i])
-        if count_multiplication != 1:
-            count += count_multiplication
-        count += count_addition
+                addition_list.append((int(col[i])))
+        if multiplication_list:
+            count += math.prod(multiplication_list)
+        count += sum(addition_list)
 
     print(count)
+
+
+part1()
 
 
 def part2():
@@ -37,7 +43,7 @@ def part2():
 
     transposed = list(map(list, zip(*input)))
     transposed.append(len(transposed[-1]) * [" "])
-    pprint(transposed)
+    # pprint(transposed)
 
     total = 0
     multiplication_list = []
@@ -47,8 +53,9 @@ def part2():
     for row in transposed:
         # Skip row if empty
         if all(ch == " " for ch in row):
-            print(multiplication_list)
-            print(addition_list)
+            # print(multiplication_list)
+            # print(addition_list)
+            # math.prod([]) on an empty list gives 1, use this to bypass that
             if multiplication_list:
                 total += math.prod(multiplication_list)
                 multiplication_list = []
